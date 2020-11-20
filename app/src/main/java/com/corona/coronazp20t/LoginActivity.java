b3fd92912307de0d1f1c69b894461f44cb13066f
rimantepomeckyte <rimantepomeckyte@users.noreply.github.com>
2020-11-20 11:16:48 +0200

    added remember me

--- app/src/main/java/com/corona/coronazp20t/LoginActivity.java
+++ app/src/main/java/com/corona/coronazp20t/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.corona.coronazp20t;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -25,6 +30,20 @@ public class LoginActivity extends AppCompatActivity {
 
         final EditText usernametext = (EditText) findViewById(R.id.usernametext); //edit text yra neapdirbtas formatas, todel isvedant reikia rasyti getText toString
         final EditText passwordtext = (EditText) findViewById(R.id.passwordtext);
+
+        final CheckBox rememberme = (CheckBox) findViewById(R.id.rememberme);
+        // bus konstruojamas vartotojo objektas perduodant context'a
+        final User user = new User(LoginActivity.this);
+        //patikriname, ar paskutini karta checkbox'as buvo pazymetas ar ne
+        rememberme.setChecked(user.isRememberedForLogin());
+
+        if (rememberme.isChecked()) {//jeigu checkbox'as buvo pazymetas - vartotojas norejo kad jo duomenys butu issaugoti
+            usernametext.setText(user.getUsernameForLogin(), TextView.BufferType.EDITABLE);
+            passwordtext.setText(user.getUsernameForLogin(), TextView.BufferType.EDITABLE);
+        } else {//jeigu checbox'as buvo nepazymetas - vartotojas nenorejo kad jo duomenys butu issaugota
+            usernametext.setText("", TextView.BufferType.EDITABLE);
+            passwordtext.setText("", TextView.BufferType.EDITABLE);
+        }
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,27 +52,35 @@ public class LoginActivity extends AppCompatActivity {
                         "Prisijungimo vardas:" + usernametext.getText().toString() + "\n" +
                              "Slaptazodis:" + passwordtext.getText().toString(),
                         Toast.LENGTH_SHORT).show();*/
-               if (Validation.isValidUsername(usernametext.getText().toString())) {
-                   //ketinimas pereiti i paieskos langa
-                   Intent goToSearchActivity=new Intent(LoginActivity.this,
-                           SearchActivity.class);
-                   startActivity(goToSearchActivity);
-               }
-               else {//kai duomenys neatitinka sablono/reikalavimo
-                   usernametext.setError(getResources().getString(R.string.login_invalid_username));
-                   usernametext.requestFocus();//sufokusuoja ties kuriuo yra klaida ir ismeta pranesima
+                if (Validation.isValidUsername(usernametext.getText().toString())) {
+                    //ketinimas pereiti i paieskos langa
+                    user.setUsernameForLogin(usernametext.getText().toString());
+                    user.setPasswordForLogin(passwordtext.getText().toString());
+                    if (rememberme.isChecked()) {
+                        user.setRemembermeKeyForLogin(true);
+                    }else{
+                        user.setRemembermeKeyForLogin(false);
+                    }
 
-               }
-               Button registerbutton = (Button) findViewById(R.id.registerbutton);
-               registerbutton.setOnClickListener(new View.OnClickListener() {
-                   @Override
-                   public void onClick(View v) {
-                       RegisterActivity();
-                   }
-               });
+                    Intent goToSearchActivity = new Intent(LoginActivity.this,
+                            SearchActivity.class);
+                    startActivity(goToSearchActivity);
+                } else {//kai duomenys neatitinka sablono/reikalavimo
+                    usernametext.setError(getResources().getString(R.string.login_invalid_username));
+                    usernametext.requestFocus();//sufokusuoja ties kuriuo yra klaida ir ismeta pranesima
+
+                }
+                Button registerbutton = (Button) findViewById(R.id.registerbutton);
+                registerbutton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        RegisterActivity();
+                    }
+                });
 
             }
-            public void RegisterActivity(){
+
+            public void RegisterActivity() {
                 Intent goRegisterActivity = new Intent(LoginActivity.this,
                         RegisterActivity.class);
                 startActivity(goRegisterActivity);
--- app/src/main/java/com/corona/coronazp20t/User.java
+++ app/src/main/java/com/corona/coronazp20t/User.java
@@ -1,10 +1,21 @@
 package com.corona.coronazp20t;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
 //nepamirsti pasirasyti pblic
 public class User {
     //1.objekto kintamieji(pozymiai, argumentai)
     private String username;
     private String password;
     private String email;
+// Shared preferences - privatus duomenys prisijungimui prie aplikacijos
+    private SharedPreferences sharedPreferences;
+    private static final String PREFERENFEC_PACKAGE_NAME="com.corona.coronazp20";
+    private static final String USERNAME_KEY="username";
+    private static final String PASSWORD_KEY="password";
+    private static final String REMEMBER_KEY="rememberMe";
+
     //2.konstruktorius
 //desiniu peles klavisu generate -> constructor -> pasizymeti visus ir ok
     public User(String username, String password, String email) {
@@ -12,30 +23,53 @@ public class User {
         this.password = password;
         this.email = email;
     }
+
+    public User(Context context){
+        this.sharedPreferences=context.getSharedPreferences(User.PREFERENFEC_PACKAGE_NAME,
+                Context.MODE_PRIVATE);
+    }
     //3.klases metodai: get ir set
 //desiniu -> generate -> getters and setters
 
-    public String getUsername() {
+    public String getUsernameForRegistration() {
         return username;
     }
 
-    public void setUsername(String username) {
+    public void setUsernameForRegistration(String username) {
         this.username = username;
     }
 
-    public String getPassword() {
+    public String getPasswordForRegistration() {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPasswordForRegistration(String password) {
         this.password = password;
     }
 
-    public String getEmail() {
+    public String getEmailForRegistration() {
         return email;
     }
 
-    public void setEmail(String email) {
+    public void setEmailForRegistration(String email) {
         this.email = email;
+    }
+    public String getUsernameForLogin(){
+        return this.sharedPreferences.getString(USERNAME_KEY, "");
+    }
+    public void setUsernameForLogin(String username){
+        this.sharedPreferences.edit().putString(USERNAME_KEY, username).commit();
+    }
+    public String getPasswordLogin(){
+        return this.sharedPreferences.getString(PASSWORD_KEY, "");
+    }
+    public void setPasswordForLogin(String password){
+        this.sharedPreferences.edit().putString(PASSWORD_KEY, password).commit();
+    }
+    public boolean isRememberedForLogin(){
+        return this.sharedPreferences.getBoolean(REMEMBER_KEY, false);
+    }
+    public void setRemembermeKeyForLogin(boolean remembermeKey){
+        this.sharedPreferences.edit().putBoolean(REMEMBER_KEY, remembermeKey).commit();
     }
 }
